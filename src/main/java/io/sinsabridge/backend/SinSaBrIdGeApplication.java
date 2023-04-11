ca6dcc88efa06ac1af8c53ff1406bf68be8cdd0e
the.matrix <dev.snoopy@gmail.com>
2023-04-11 22:16:17 +0900

    start

---
+++ src/main/java/io/sinsabridge/backend/SinSaBrIdGeApplication.java
@@ -0,0 +1,13 @@
+package io.sinsabridge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinSaBrIdGeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinSaBrIdGeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sinsabridge/backend/SinSaBrIdGeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sinsabridge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinSaBrIdGeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
